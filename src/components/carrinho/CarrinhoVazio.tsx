b874790bbabcfafb057b598f49fc5aa482c28cce
thiagokassabian <thiago.kassabian@gmail.com>
2024-07-22 15:11:51 -0300

    últimos ajustes

--- src/components/carrinho/CarrinhoVazio.tsx
+++ src/components/carrinho/CarrinhoVazio.tsx
@@ -7,7 +7,7 @@ export default function CarrinhoVazio() {
 			<IconShoppingCartOff size={150} stroke={0.5} />
 			<h2 className="text-2xl font-bold">Carrinho vazio</h2>
 			<p>Adicione produtos ao carrinho para continuar</p>
-			<Link href="/" className="bg-green-800 text-white px-3 py-2 rounded">
+			<Link href="/" className="bg-green-800 text-white px-4 py-2 rounded">
 				Ver produtos
 			</Link>
 		</div>
--- src/components/template/Button.tsx
+++ src/components/template/Button.tsx
@@ -8,9 +8,9 @@ export default function Button(props: ButtonProps) {
 	return (
 		<button
 			className={`
-				rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans
-				text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20
-				transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85]
+				rounded-full border py-1 px-5 text-center align-middle
+				text-xs uppercase text-white shadow-md shadow-gray-500/20
+				transition-all hover:shadow-lg hover:shadow-gray-500/40 focus:opacity-[0.85]
 				focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none
 				disabled:opacity-50 disabled:shadow-none ${props.className}`}
 			onClick={props.onClick}>
